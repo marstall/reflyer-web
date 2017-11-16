c17d53b70c7fb9a2527ed6b03c6686f32c7ac324
marstall <marstall@gmail.com>
2017-11-15 20:37:15 -0500

    push

--- app/models/push_notification.rb
+++ app/models/push_notification.rb
@@ -83,7 +83,7 @@ class PushNotification < ActiveRecord::Base
   end
   
   def send_to_all_users
-    users = User.where.not(expo_push_token:nil).where.not(expo_push_token:"null")
+    users = User.where.not(expo_push_token:nil).where.not(expo_push_token:"null").where(notifications_permission_state:"accepted")
     puts "sending push to all #{users.length} users ..."
     @expo_push_tokens = users.map{|user|user.expo_push_token}
     actually_send_push_notifications
