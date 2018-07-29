fb71468fd1262380987a24b0ae825bb81922e970
marstall <marstall@gmail.com>
2018-07-29 12:22:18 -0400

    subscribe form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'home#home3'
-  get 'home', to: 'home#home'
   get 'email', to: 'flyers#email'
   get 'blob', to: 'flyers#blob'
   get 'flyer/:id/image/:size', to: 'flyers#image'
